0000b13498c7dcf6de537e66da700e72c8c7ef6d
Aimers2023 <132036897+Aimers2023@users.noreply.github.com>
2023-04-28 11:13:36 +0530

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,21 +1,49 @@
-from typing import TYPE_CHECKING, Type
+from __future__ import absolute_import
 
-from uvicorn.supervisors.basereload import BaseReload
-from uvicorn.supervisors.multiprocess import Multiprocess
+# For backwards compatibility, provide imports that used to be here.
+from .connection import is_connection_dropped
+from .request import SKIP_HEADER, SKIPPABLE_HEADERS, make_headers
+from .response import is_fp_closed
+from .retry import Retry
+from .ssl_ import (
+    ALPN_PROTOCOLS,
+    HAS_SNI,
+    IS_PYOPENSSL,
+    IS_SECURETRANSPORT,
+    PROTOCOL_TLS,
+    SSLContext,
+    assert_fingerprint,
+    resolve_cert_reqs,
+    resolve_ssl_version,
+    ssl_wrap_socket,
+)
+from .timeout import Timeout, current_time
+from .url import Url, get_host, parse_url, split_first
+from .wait import wait_for_read, wait_for_write
 
-if TYPE_CHECKING:
-    ChangeReload: Type[BaseReload]
-else:
-    try:
-        from uvicorn.supervisors.watchfilesreload import (
-            WatchFilesReload as ChangeReload,
-        )
-    except ImportError:  # pragma: no cover
-        try:
-            from uvicorn.supervisors.watchgodreload import (
-                WatchGodReload as ChangeReload,
-            )
-        except ImportError:
-            from uvicorn.supervisors.statreload import StatReload as ChangeReload
-
-__all__ = ["Multiprocess", "ChangeReload"]
+__all__ = (
+    "HAS_SNI",
+    "IS_PYOPENSSL",
+    "IS_SECURETRANSPORT",
+    "SSLContext",
+    "PROTOCOL_TLS",
+    "ALPN_PROTOCOLS",
+    "Retry",
+    "Timeout",
+    "Url",
+    "assert_fingerprint",
+    "current_time",
+    "is_connection_dropped",
+    "is_fp_closed",
+    "get_host",
+    "parse_url",
+    "make_headers",
+    "resolve_cert_reqs",
+    "resolve_ssl_version",
+    "split_first",
+    "ssl_wrap_socket",
+    "wait_for_read",
+    "wait_for_write",
+    "SKIP_HEADER",
+    "SKIPPABLE_HEADERS",
+)
